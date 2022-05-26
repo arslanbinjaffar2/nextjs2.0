db340cbf36259c860ccdd924009918a9ac031bd4
moazzam_eventbuizz <mms@eventbuizz.com>
2022-05-26 19:13:09 +0500

    updated

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', '@babel/preset-typescript'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@src': './src',
+          },
+          extensions: [
+            '.ios.js',
+            '.android.js',
+            '.js',
+            '.jsx',
+            '.json',
+            '.tsx',
+            '.ts',
+            '.native.js',
+            '.json'
+          ],
+        }
+      ]
+    ]
   };
 };
