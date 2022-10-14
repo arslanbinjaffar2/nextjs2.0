34ec859e50800a0afcee3d4bf0b9dcfc77a35884
moazzam_eventbuizz <mms@eventbuizz.com>
2022-10-14 12:45:09 +0500

    Revert "updated"
    This reverts commit 4c85b0afaafb39519468b2e5f473ecbd28e75310.

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import {AppRegistry} from 'react-native'
-import App from "@src/App";
-
-AppRegistry.registerComponent("Ssss", () => App)
-
-AppRegistry.runApplication("Ssss", {
-  initialProps: {},
-  rootTag: document.getElementById('root'),
-})
