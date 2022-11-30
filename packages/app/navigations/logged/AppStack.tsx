43b3c2dfd66bd0cac345d2cc95d913456ea12826
moazzam_eventbuizz <mms@eventbuizz.com>
2022-11-30 19:17:17 +0500

    live change

--- packages/app/navigations/logged/AppStack.tsx
+++ packages/app/navigations/logged/AppStack.tsx
@@ -1,7 +1,6 @@
 
 import * as React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import { Platform } from 'react-native';
 import Index from 'app/screens/mobile/dashboard/Index';
 import colors from 'app/styles/colors';
 import DrawerLayout from 'app/containers/mobile/DrawerLayout';
@@ -10,18 +9,16 @@ const Drawer = createDrawerNavigator();
 
 const AppStack = () => {
   return (
-    Platform.OS !== 'web' && (
-      <Drawer.Navigator
-        screenOptions={{
-          drawerStyle: {
-            backgroundColor: `rgba(${colors.darkbox},0.95)`,
-            width: '80%'
-          }
-        }}
-        drawerContent={(props: any) => <DrawerLayout {...props} />}>
-        <Drawer.Screen options={{ headerShown: false }} name="index" component={Index} />
-      </Drawer.Navigator>
-    )
+    <Drawer.Navigator
+      screenOptions={{
+        drawerStyle: {
+          backgroundColor: `rgba(${colors.darkbox},0.95)`,
+          width: '80%'
+        }
+      }}
+      drawerContent={(props: any) => <DrawerLayout {...props} />}>
+      <Drawer.Screen options={{ headerShown: false }} name="index" component={Index} />
+    </Drawer.Navigator>
   )
 }
 
