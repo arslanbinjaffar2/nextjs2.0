0ed39deda4e1db3cdc343e6ff43352fe464bcb31
moazzam_eventbuizz <mms@eventbuizz.com>
2023-05-05 18:57:33 +0500

    live change

--- application/navigations/logged/AppStack.tsx
+++ application/navigations/logged/AppStack.tsx
@@ -3,8 +3,35 @@ import * as React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Index from 'application/screens/mobile/dashboard/Index';
 import Program from 'application/screens/mobile/program/Index';
+import Attendee from 'application/screens/mobile/attendee/Index';
+import Speaker from 'application/screens/mobile/speaker/Index';
+import NewsUpdate from 'application/screens/mobile/news_update/Index';
+import Document from 'application/screens/mobile/document/Index';
+import MyDocument from 'application/screens/mobile/document/MyDocument';
+import Map from 'application/screens/mobile/map/Index';
+import Chat from 'application/screens/mobile/chat/Index';
+import Gallery from 'application/screens/mobile/gallery/Index';
+import Sponsor from 'application/screens/mobile/sponsor/Index';
+import Exhibitor from 'application/screens/mobile/exhibitor/Index';
+import InformationPage from 'application/screens/mobile/information_page/Index';
+import Poll from 'application/screens/mobile/poll/Index';
+import Survey from 'application/screens/mobile/survey/Index';
+import QA from 'application/screens/mobile/qa/Index';
+import CheckIn from 'application/screens/mobile/checkin/Index';
+import SocialMedia from 'application/screens/mobile/social_media/Index';
+import HelpDesk from 'application/screens/mobile/helpdesk/Index';
+import FloorPlan from 'application/screens/mobile/floorplan/Index';
+import NetworkInterest from 'application/screens/mobile/network_interest/Index';
+import RequestToSpeak from 'application/screens/mobile/request_to_speak/Index';
+import MyEvent from 'application/screens/mobile/event/MyEvent';
+import UpcomingEvent from 'application/screens/mobile/event/UpcomingEvent';
+import SocialWall from 'application/screens/mobile/social_wall/Index';
 import colors from 'application/styles/colors';
 import DrawerLayout from 'application/screens/mobile/layouts/Drawer';
+import MyProgram from 'application/screens/mobile/program/MyProgram';
+import SessionCheckIn from 'application/screens/mobile/program/SessionCheckIn';
+import MyAttendee from 'application/screens/mobile/attendee/MyAttendee';
+import MyNote from 'application/screens/mobile/notes/MyNote';
 
 const Drawer = createDrawerNavigator();
 
@@ -17,9 +44,37 @@ const AppStack = () => {
           width: '80%'
         }
       }}
+      initialRouteName="dashboard"
       drawerContent={(props: any) => <DrawerLayout {...props} />}>
       <Drawer.Screen options={{ headerShown: false }} name="dashboard" component={Index} />
       <Drawer.Screen options={{ headerShown: false }} name="agendas" component={Program} />
+      <Drawer.Screen options={{ headerShown: false }} name="myagendas" component={MyProgram} />
+      <Drawer.Screen options={{ headerShown: false }} name="checin_agendas" component={SessionCheckIn} />
+      <Drawer.Screen options={{ headerShown: false }} name="attendees" component={Attendee} />
+      <Drawer.Screen options={{ headerShown: false }} name="myattendees" component={MyAttendee} />
+      <Drawer.Screen options={{ headerShown: false }} name="speakers" component={Speaker} />
+      <Drawer.Screen options={{ headerShown: false }} name="alerts" component={NewsUpdate} />
+      <Drawer.Screen options={{ headerShown: false }} name="ddirectory" component={Document} />
+      <Drawer.Screen options={{ headerShown: false }} name="mydocuments" component={MyDocument} />
+      <Drawer.Screen options={{ headerShown: false }} name="maps" component={Map} />
+      <Drawer.Screen options={{ headerShown: false }} name="chat" component={Chat} />
+      <Drawer.Screen options={{ headerShown: false }} name="gallery" component={Gallery} />
+      <Drawer.Screen options={{ headerShown: false }} name="information_pages" component={InformationPage} />
+      <Drawer.Screen options={{ headerShown: false }} name="sponsors" component={Sponsor} />
+      <Drawer.Screen options={{ headerShown: false }} name="exhibitors" component={Exhibitor} />
+      <Drawer.Screen options={{ headerShown: false }} name="polls" component={Poll} />
+      <Drawer.Screen options={{ headerShown: false }} name="survey" component={Survey} />
+      <Drawer.Screen options={{ headerShown: false }} name="qa" component={QA} />
+      <Drawer.Screen options={{ headerShown: false }} name="social" component={SocialMedia} />
+      <Drawer.Screen options={{ headerShown: false }} name="checkIn" component={CheckIn} />
+      <Drawer.Screen options={{ headerShown: false }} name="help_desk" component={HelpDesk} />
+      <Drawer.Screen options={{ headerShown: false }} name="plans" component={FloorPlan} />
+      <Drawer.Screen options={{ headerShown: false }} name="business" component={NetworkInterest} />
+      <Drawer.Screen options={{ headerShown: false }} name="myturnlist" component={RequestToSpeak} />
+      <Drawer.Screen options={{ headerShown: false }} name="homeMyevents" component={MyEvent} />
+      <Drawer.Screen options={{ headerShown: false }} name="social_wall" component={SocialWall} />
+      <Drawer.Screen options={{ headerShown: false }} name="my_notes" component={MyNote} />
+      <Drawer.Screen options={{ headerShown: false }} name="upcomingEvents" component={UpcomingEvent} />
     </Drawer.Navigator>
   )
 }
