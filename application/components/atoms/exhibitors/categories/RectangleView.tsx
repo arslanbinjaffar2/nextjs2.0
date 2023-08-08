18e8905a49e9b5e8c901842baa2ecdc5ac21f933
moazzam_eventbuizz <mms@eventbuizz.com>
2023-08-08 13:51:58 +0500

    live change

--- application/components/atoms/exhibitors/categories/RectangleView.tsx
+++ application/components/atoms/exhibitors/categories/RectangleView.tsx
@@ -15,7 +15,7 @@ const RectangleView = ({ k, category, updateTab }: AppProps) => {
     const { FetchExhibitors } = UseExhibitorService();
 
     return (
-        <Box w="100%" key={k} borderBottomWidth={k === 3 ? 0 : 1} borderColor="primary.text" py="3">
+        <Box w="100%" key={k} borderBottomWidth={1} borderColor="primary.text" py="3">
             <Pressable
                 onPress={() => {
                     FetchExhibitors({ category_id: category.id, query: '' });
