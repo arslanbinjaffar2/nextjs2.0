5ae6e16866cb5a623c782ecd25ce92fc018a089f
ahmad13054 <mah@eventbuizz.com>
2023-11-08 12:45:36 +0500

    master

--- application/components/atoms/polls/RectangleView.tsx
+++ application/components/atoms/polls/RectangleView.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
 import { Box, HStack, Spacer, Text, VStack, Pressable, Icon } from 'native-base'
 import { Poll } from 'application/models/poll/Poll';
 import UseEventService from 'application/store/services/UseEventService';
@@ -22,6 +23,9 @@ const RectangleView = ({poll, completed}:{poll:Poll, completed:boolean}) => {
        }}>
       <Box w="100%" borderBottomWidth='1' borderColor="primary.text" py="3">
         <HStack px="3" w="100%" space="0" alignItems="center" justifyContent="space-between">
+          <VStack marginRight={5}>
+            <Icon size="lg" as={FontAwesome} name={poll?.agenda_favs?.length > 0 ? 'star' :'star-o'} color={poll?.agenda_favs?.length > 0 ? 'primary.400' : 'primary.text'} />
+          </VStack>
           <VStack bg="red" w="100%" maxW={['95%', '80%', '70%']} space="1">
             <Text fontSize="md">{poll.program.info.topic}</Text>
             <Text fontSize="sm">{moment(`${poll.program.start_date} ${poll.program.start_time}`).format('HH:ss')} - {moment(`${poll.program.start_date} ${poll.end_time}`).format('HH:ss')} </Text>
