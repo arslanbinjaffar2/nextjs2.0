1b180c1df41d5c698fc5dfb2f8ce78c59a8aac1b
hashmat_eventbuizz <hab@eventbuizz.com>
2024-01-09 11:31:40 +0500

    master

--- application/components/atoms/polls/RectangleView.tsx
+++ application/components/atoms/polls/RectangleView.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SimpleLineIcons from '@expo/vector-icons/SimpleLineIcons'
-import FontAwesome from '@expo/vector-icons/FontAwesome'
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { Box, HStack, Spacer, Text, VStack, Pressable, Icon } from 'native-base'
 import { Poll } from 'application/models/poll/Poll';
 import UseEventService from 'application/store/services/UseEventService';
@@ -30,7 +30,7 @@ const RectangleView = ({poll, completed, settings}:{poll:Poll, completed:boolean
       <Box w="100%" borderBottomWidth='1' borderColor="primary.text" py="3">
         <HStack px="3" w="100%" space="0" alignItems="center" justifyContent="space-between">
           <VStack marginRight={5}>
-            <Icon size="lg" as={FontAwesome} name={poll?.agenda_favs?.length > 0 ? 'star' :'star-o'} color={poll?.agenda_favs?.length > 0 ? 'primary.400' : 'primary.text'} />
+            <Icon size="lg" as={AntDesign} name={poll?.agenda_favs?.length > 0 ? 'heart' :'hearto'} color={poll?.agenda_favs?.length > 0 ? 'primary.400' : 'primary.text'} />
           </VStack>
           <VStack bg="red" w="100%" maxW={['95%', '80%', '70%']} space="1">
             <Text fontSize="md">{poll.program.info.topic}</Text>
