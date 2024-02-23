bb3fa5095436484b2a1e9b7734b6da5a5364e7a7
Hamza Latif <hla@eventbuizz.com>
2024-02-23 19:41:12 +0500

    WA20-113a

--- application/components/atoms/banners/BannerAds.tsx
+++ application/components/atoms/banners/BannerAds.tsx
@@ -1,50 +1,67 @@
-
 import React, { useEffect, useState } from 'react'
-import { Image } from 'native-base'
+import { Image, View } from 'native-base'
 import { Banner } from 'application/models/Banner'
 import UseEnvService from 'application/store/services/UseEnvService'
 import UseBannerService from 'application/store/services/UseBannerService'
- const BannerAds = ({
-    module_name,
-    module_type,
-  }: {
-    module_name: string
-    module_type: string
-  }) => {
+import { Platform, TouchableOpacity } from 'react-native'
+import { useRouter } from 'solito/router'
+import UseEventService from '../../../store/services/UseEventService'
+const BannerAds = ({
+ module_name,
+ module_type,
+}: {
+  module_name: string
+  module_type: string
+}) => {
 
- const { _env } = UseEnvService()
-
+  const { _env } = UseEnvService()
+  const { event } = UseEventService();
   const [filteredBanners, setFilteredBanners] = useState<Banner[]>([])
   const { banners, FetchBanners } = UseBannerService()
   useEffect(() => {
     const filteredBanner = banners.filter((banner: Banner) => {
-       return (
-         banner.module_name === module_name && banner.module_type === module_type
-       )
-     })
-     setFilteredBanners(filteredBanner)
-   }, [banners])
+      return (
+        ((banner.module_name === module_name && banner.module_type === module_type) || banner.banner_position === module_type )
+      )
+    })
+    setFilteredBanners(filteredBanner)
+  }, [banners])
   React.useEffect(() => {
-     FetchBanners()
-   }, [])
+    FetchBanners()
+  }, [])
+  const { push } = useRouter()
+  const handleBannerClick = (banner: Banner) => {
+    if (banner.module_name === 'sponsors') {
+      push(`/${event.url}/${banner.module_name}/detail/${banner.sponsor_id}`);
+    } else if(banner.module_name === 'exhibitors'){
+      push(`/${event.url}/${banner.module_name}/detail/${banner.exhibitor_id}`);
+    }else if(banner.module_name === 'agendas'){
+      push(`/${event.url}/${banner.module_name}/detail/${banner.agenda_id}`);
+    } else if (banner.other_link_url) {
+      push(`/${event.url}/${banner.other_link_url}`);
+    }
+  };
   return (
     <>
-        {filteredBanners.map(
-          (banner, index) => (
-          console.log(banner.image),
-            (
-              <Image
-                key={index}
-                source={{
-                  uri: `${_env.eventcenter_base_url}/assets/banners/${banner.image}`,
-                }}
-                alt="Banner Image"
-                width="100%"
-                height="100%"
-              />
-              )
-          )
-        )}
+      {filteredBanners.map(
+        (banner, index) => (
+          <View
+            key={index}
+            onClick={() => handleBannerClick(banner)}
+            style={{ width: '100%', height: '100%' }}
+          >
+            <Image
+              key={index}
+              source={{
+                uri: `${_env.eventcenter_base_url}/assets/banners/${banner.image}`,
+              }}
+              alt="Banner Image"
+              width="100%"
+              height="100%"
+            />
+          </View>
+        )
+      )}
     </>
     )
   }
