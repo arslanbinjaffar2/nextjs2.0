e28e1e2c2a784897ad2deef684336b963e6906df
Eventbuizz <info@eventbuizz.com>
2024-02-28 22:48:44 -0700

    no message

--- application/components/templates/social-wall/Index.tsx
+++ application/components/templates/social-wall/Index.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { Box, Image } from 'native-base'
 import SquareBox from 'application/components/atoms/social-wall/SquareBox';
 import AddPost from 'application/components/atoms/social-wall/AddPost';
-import PostListing from 'application/components/atoms/social-wall/PostListing';
 import BannerAds from 'application/components/atoms/banners/BannerAds'
 
 const Index = () => {
@@ -11,11 +10,7 @@ const Index = () => {
   return (
     <>
       <AddPost />
-      <Box w="100%">
-        <SquareBox />
-        <SquareBox />
-        <SquareBox />
-      </Box>
+
       <Box width={"100%"} height={"5%"}>
         <BannerAds module_name={'social_wall'} module_type={'listing'} />
       </Box>
