4915d8e39f79f10d4125a662ee4a1101b9a1f78b
Hamza Latif <hla@eventbuizz.com>
2024-03-18 17:07:30 +0500

    WA20-1100

--- application/components/templates/attendees/web/Index.tsx
+++ application/components/templates/attendees/web/Index.tsx
@@ -271,7 +271,7 @@ const Index = ({ speaker, screen }: Props) => {
                                 w={'50%'} 
                                 _text={{ fontWeight: '600' }}
                             >
-                                {event?.labels?.GENERAL_ALL}
+                                {event?.labels?.SPEAKER_NAME}
                             </Button>
                         }
                         {( event?.speaker_settings?.tab == 1) &&
@@ -290,7 +290,7 @@ const Index = ({ speaker, screen }: Props) => {
                                 w={'50%'} 
                                 _text={{ fontWeight: '600' }}
                             >
-                                CATEGORIES
+                                {event?.labels?.SPEAKER_CATEGORY}
                             </Button>
                         }
                     </HStack>}
