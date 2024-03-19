391e95aeb58822ac582f68bb6acbb2d36576fd1b
Hamza Latif <hla@eventbuizz.com>
2024-03-19 16:29:08 +0500

    WA20-150

--- application/components/atoms/attendees/detail/BasicInfoBlock.tsx
+++ application/components/atoms/attendees/detail/BasicInfoBlock.tsx
@@ -46,7 +46,7 @@ const BasicInfoBlock = ({ detail, showPrivate, speaker }: AppProps) => {
                         </Text>
                         {detail?.detail?.info &&
                             (detail?.detail?.info.company_name ||
-                                detail?.detail?.info.title) && 
+                                detail?.detail?.info.title) &&
                                 (showPrivate == 1 || (isPrivate?.title == 0 || isPrivate?.company_name == 0))
                                 && (
                                 <>
@@ -54,7 +54,7 @@ const BasicInfoBlock = ({ detail, showPrivate, speaker }: AppProps) => {
                                             detail?.detail?.info?.title &&
                                             ", "}
                                             {detail?.detail?.info?.company_name && detail?.detail?.info?.company_name}</Text>
-                                    
+
                                 </>
                             )}
                         {(showPrivate == 1 || isPrivate?.department == 0) && detail?.detail?.info?.department && (
@@ -101,8 +101,7 @@ const BasicInfoBlock = ({ detail, showPrivate, speaker }: AppProps) => {
             {detail?.detail?.attendee_cv && (
             <Box w="100%" bg="primary.secondary" px="5" py="3" borderTopWidth="1" borderColor="primary.darkbox">
                 <HStack w="100%" space="0">
-                    {(showPrivate == 1 || isPrivate?.resume == 0) && detail?.detail?.attendee_cv && (speaker == 0 || detail?.speaker_setting.resume == 1) && <Center w="20%" borderRightWidth={showPrivate == 1 && (detail?.show_hotel_management == 1 || detail?.show_hotels == 1) ? '1' : '0'} borderColor={'primary.box'} alignItems="flex-start">
-                        <Pressable
+                    {(showPrivate == 1 || isPrivate?.resume == 0) && detail?.detail?.attendee_cv && (speaker == 0 || speaker == 1 || detail?.speaker_setting.resume == 1) && <Center w="20%" borderRightWidth={showPrivate == 1 && (detail?.show_hotel_management == 1 || detail?.show_hotels == 1) ? '1' : '0'} borderColor={'primary.box'} alignItems="flex-start"><Pressable
                             onPress={async () => {
                                 const url: any = `${_env.eventcenter_base_url}/assets/attendees/cv/${detail?.detail?.attendee_cv}`;
                                 const supported = await Linking.canOpenURL(url);
