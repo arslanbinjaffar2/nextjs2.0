84433f2965cc4ead2f9bc5bee71966b7706d9d7e
M Irfan <mir@eventbuizz.com>
2024-03-25 16:31:31 +0500

    WA20-723

--- application/screens/web/layouts/RightBar.tsx
+++ application/screens/web/layouts/RightBar.tsx
@@ -18,6 +18,8 @@ import UseBannerService from 'application/store/services/UseBannerService'
 import UseSponsorService from 'application/store/services/UseSponsorService'
 import UseDocumentService from 'application/store/services/UseDocumentService'
 import UseEnvService from 'application/store/services/UseEnvService'
+import UseLoadingService from 'application/store/services/UseLoadingService';
+import in_array from "in_array";
 type ScreenParams = { id: string, cms: string | undefined }
 
 const RightBar = () => {
@@ -25,6 +27,7 @@ const RightBar = () => {
   const { _env } = UseEnvService()
   const { clearState, documents } = UseDocumentService();
   const { sponsors, FetchSponsorContact } = UseSponsorService();
+  const { scroll, processing } = UseLoadingService();
   // const handleSponsorContactClick = (attendeeId: any) => {
   //   console.log(attendeeId,'here');
   //     FetchSponsorContact({ id: Number(attendeeId) });
@@ -42,10 +45,10 @@ const RightBar = () => {
     <>
 
       {nextRouter.asPath.includes('exhibitors/detail') &&  <ExhibitorContactInfo />}
-      {nextRouter.asPath.includes('exhibitors/detail') && event?.exhibitor_settings?.notes == 1 &&  <ExhibitorNotesBox />}
+      {nextRouter.asPath.includes('exhibitors/detail') && event?.exhibitor_settings?.notes == 1 && !in_array('exhibitor-detail', processing) &&  <ExhibitorNotesBox />}
       {nextRouter.asPath.includes('sponsors/detail') &&  <SponsorContactInfo />}
-      {nextRouter.asPath.includes('sponsors/detail') && event?.sponsor_settings?.notes == 1 &&  <SponsorNotesBox />}
-      {nextRouter.asPath.includes('agendas/detail') && event?.agenda_settings?.enable_notes == 1 &&  <ProgramNotesBox />}
+      {nextRouter.asPath.includes('sponsors/detail') && event?.sponsor_settings?.notes == 1 && !in_array('sponsor-detail', processing) && <SponsorNotesBox />}
+      {nextRouter.asPath.includes('agendas/detail') && event?.agenda_settings?.enable_notes == 1 && !in_array('program-detail', processing) && <ProgramNotesBox />}
       {(nextRouter.asPath.includes('speakers/detail') || nextRouter.asPath.includes('attendees/detail')) && ((detail?.detail?.info?.facebook && detail?.field_setting?.facebook) || (detail?.detail?.info?.twitter && detail?.field_setting?.twitter) || (detail?.detail?.info?.linkedin && detail?.field_setting?.linkedin) || (detail?.detail?.info?.website && detail?.field_setting?.website)) && <ContactInfo detail={detail} />}
       {nextRouter.asPath.includes('agendas/detail') && event?.agenda_settings?.session_ratings == 1 &&  <SessionRating />}
       {(nextRouter.asPath.includes('speakers/detail') || nextRouter.asPath.includes('attendees/detail')) && ((detail?.detail?.info?.facebook && detail?.field_setting?.facebook) || (detail?.detail?.info?.twitter && detail?.field_setting?.twitter) || (detail?.detail?.info?.linkedin && detail?.field_setting?.linkedin) || (detail?.detail?.info?.website && detail?.field_setting?.website) || (detail?.setting?.contact_vcf && detail?.setting?.contact_vcf)) && <ContactInfo detail={detail} />}
