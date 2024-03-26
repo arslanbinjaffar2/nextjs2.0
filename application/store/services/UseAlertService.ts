5f12a5f5ab204ec3eacbed0ab767c88576d1574e
khawarr <khu@eventbuizz.com>
2024-03-26 17:05:50 +0500

    WA20-546

--- application/store/services/UseAlertService.ts
+++ application/store/services/UseAlertService.ts
@@ -1,6 +1,6 @@
 import { useCallback } from 'react'
 
-import { SelectAlerts, SelectAttendeeAlerts, AlertActions, SelectAlertDetail, SelectAlertSetting  } from 'application/store/slices/Alert.Slice'
+import { SelectAlerts, SelectAttendeeAlerts, AlertActions, SelectAlertDetail, SelectAlertSetting, SelectUnreadCount  } from 'application/store/slices/Alert.Slice'
 
 import {  Alert, AlertSetting } from 'application/models/alert/Alert'
 
@@ -9,12 +9,14 @@ import { useAppDispatch, useAppSelector } from 'application/store/Hooks'
 export type AlertServiceOperators = {
     alerts: Alert[],
     detail: Alert | null,
+    unread: number,
     attendee_alerts: "" | number[],
     alert_setting: null | AlertSetting[],
     FetchAlerts: () => void,
     markAlertRead: (payload:{alertIds:string}) => void,
     FetchAlertDetails: (payload:{alertId:number}) => void,
     MarkAlertAsRead: (payload:{alertId:number}) => void,
+    setUnreadCount: (payload:number) => void,
 }
 
 /**
@@ -30,6 +32,7 @@ export const UseAlertService = (): Readonly<AlertServiceOperators> => {
         alerts: useAppSelector(SelectAlerts),
         alert_setting: useAppSelector(SelectAlertSetting),
         attendee_alerts: useAppSelector(SelectAttendeeAlerts),
+        unread: useAppSelector(SelectUnreadCount),
         FetchAlerts: useCallback(
             () => {
                 dispatch(AlertActions.FetchAlerts())
@@ -51,6 +54,12 @@ export const UseAlertService = (): Readonly<AlertServiceOperators> => {
         MarkAlertAsRead: useCallback(
             (payload:{alertId:number}) => {
               dispatch(AlertActions.MarkAlertAsRead(payload));
+            },
+            [dispatch],
+          ),
+        setUnreadCount: useCallback(
+            (payload) => {
+              dispatch(AlertActions.setUnreadCount(payload));
             },
             [dispatch],
           ),
