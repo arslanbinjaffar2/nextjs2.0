7fbc7ef9a7c94f6583ed6ffc244d3b27d46ad15b
Arslan jaffar <aja@eventbuizz.com>
2024-03-26 17:03:08 +0500

    WA20-1243

---
+++ application/assets/icons/close.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import Svg, { G, Path,SvgProps } from "react-native-svg";
+const SVGComponent = (props:SvgProps) => (
+  <Svg
+    width={props.width}
+    height={props.height}
+    viewBox="0 0 14 14"
+    {...props}
+  >
+    <G id="cancel_1_" data-name="cancel (1)" transform="translate(0 -0.001)">
+      <G id="Group_638" data-name="Group 638" transform="translate(0 0.001)">
+        <Path
+          id="Path_279"
+          data-name="Path 279"
+          d="M8.042,7l5.742-5.742A.737.737,0,1,0,12.742.217L7,5.959,1.258.217A.737.737,0,0,0,.216,1.259L5.958,7,.216,12.743a.737.737,0,1,0,1.042,1.042L7,8.043l5.742,5.742a.737.737,0,1,0,1.042-1.042Z"
+          transform="translate(0 -0.001)"
+          fill="#fff"
+        />
+      </G>
+    </G>
+  </Svg>
+);
+export default SVGComponent;
--- application/screens/web/events/UpcomingEvent.tsx
+++ application/screens/web/events/UpcomingEvent.tsx
@@ -3,17 +3,52 @@ import PropTypes from 'prop-types';
 import Master from 'application/screens/web/layouts/Master';
 import UseEventService from 'application/store/services/UseEventService';
 import NextBreadcrumbs from 'application/components/atoms/NextBreadcrumbs';
+import ToastContainer ,{Status} from 'application/components/atoms/toast/index';
+import {Button, Toast } from 'native-base';
+
 
 type indexProps = {
     navigation: unknown
 }
 
 const Index = ({ navigation }: indexProps) => {
+    const [ONoff,setOnOff]=React.useState(false)
     const { modules } = UseEventService();
     const module = modules.find((module) => module.alias === 'upcomingEvents');
+    // const { showToast,Toast } = useToast();
+
+    // const handleSuccess = () => {
+    //   showToast(Status.Success, 'Action completed successfully!');
+    // };
+  
+    // const handleError = () => {
+    //   showToast(Status.Error, 'An error occurred!');
+    // };
+    React.useEffect(() => {
+        if (ONoff) {
+            Toast.show(
+                {
+                    render: () =>(
+                        <ToastContainer
+                        setShow={setOnOff}
+                        status={Status.Success}
+                        message='asaaasasasaasas'
+                        />
+                        )
+                        
+                    })
+                    
+                }
+      
+    }, [ONoff])
+    
     return (
         <>
             <NextBreadcrumbs module={module} />
+            <Button onPress={()=>setOnOff(true)} >
+           Show Success Toast
+            </Button>
+
         </>
     );
 };
