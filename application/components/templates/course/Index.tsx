03baaaf5a9da3ab12db61835e1ffd9899685f800
Arslan jaffar <aja@eventbuizz.com>
2024-03-28 16:43:17 +0500

    WA20-744

--- application/components/templates/course/Index.tsx
+++ application/components/templates/course/Index.tsx
@@ -9,7 +9,7 @@ const Index = () => {
   const [service, setService] = useState("")
   const { modules } = UseEventService();
   const module = modules.find((module) => module.alias === 'Course');
-  
+  const [mode,setMode]=React.useState("list")
   return (
     <>
       <NextBreadcrumbs module={module} />
@@ -33,34 +33,38 @@ const Index = () => {
         {/*  */}
         <HStack w="100%" mb="3" space="1" alignItems="center" justifyContent="flex-end">
           <IconButton
-            p="0"
+            p={mode=="list"?1:0}
             variant="transparent"
             icon={<Icon size="xl" as={Entypo} name="menu" color="primary.text" />}
+            onPress={()=>setMode('list')}
           />
           <IconButton
-            p="0"
+            p={mode=="grid"?1:0}
             variant="transparent"
-            icon={<Icon size="xl" as={Entypo} name="grid" color="primary.text" />}
+            icon={<Icon size="xl" as={Entypo} name="grid" color="primary.text" 
+            onPress={()=>setMode('grid')}
+            />}
           />
         </HStack>
         {/*  */}
-        <HStack w="100%" mb="3" space="1" bg="primary.box" rounded={'md'} alignItems="center" flexDirection={'column'} justifyContent="flex-end">
+     {mode=='list'&&  <HStack w="100%" mb="3" space="1" bg="primary.box" rounded={'md'} alignItems="center" flexDirection={'column'} justifyContent="flex-end">
           {new Array(5).fill('').map((item,index,arr)=>{
             return(
         <Box pt={'20px'} pb={'14px'} px={'16px'} w={'100%'}  flexDirection={'row'} alignItems={'center'}
-        borderBottomColor={'white'} borderBottomWidth={arr.length-1==index?'0px':'1px'}
+        borderBottomColor={'primary.border'} borderBottomWidth={arr.length-1==index?'0px':'1px'}
         >
-          <Text position={'absolute'} zIndex={'9999'} top={'8'} left={'5'} w={'60px'} fontSize="xs" fontWeight={'bold'} bg={'black'} borderRadius={'13px'} px={'10px'} py={'4px'}>popular</Text>
+          <Text position={'absolute'} textTransform={'uppercase'} zIndex={'9999'} top={'8'} left={'5'}  fontSize="xs" fontWeight={'semibold'} bg={'black'} borderRadius={'13px'} px={'10px'} py={'4px'}>popular</Text>
         <Image 
         position={'relative'}
         borderRadius={'10px'}
         w={'128px'}
+        height={'140px'}
         source={{
       uri: "https://wallpaperaccess.com/full/317501.jpg"
-    }} alt="Alternate Text" size="xl" />
+    }} alt="Alternate Text"  />
     <View marginLeft={'16px'}>
-      <Text  fontSize="lg" fontWeight={'bold'}>How finding and sharing information online has evolved…</Text>
-      <Text opacity={'1'} fontSize="xs" fontWeight={'medium'} color={'#000000'} my={'4px'} bg="primary.box" w={'55px'} borderRadius={'13px'} textAlign={'center'} p={'1'}>42 min</Text>
+      <Text  fontSize="lg" fontWeight={'semibold'}>How finding and sharing information online has evolved…</Text>
+      <Text opacity={'1'} fontSize="xs" fontWeight={'medium'} color={'primary.text'} my={'4px'} bg="primary.box" w={'55px'} borderRadius={'13px'} textAlign={'center'} p={'1'}>42 min</Text>
       <Text  fontSize="sm" fontWeight={'medium'}>By : Doug Rose</Text>
       
     </View>
@@ -68,7 +72,34 @@ const Index = () => {
         </Box>
             )
           })}
-        </HStack>
+        </HStack>}
+     {mode=='grid'&&   <HStack display={'flex'} mb="3" pt="2" w="100%" space="0" alignItems="center" flexDirection={'row'} justifyContent={'space-between'} flexWrap={'wrap'}
+        >
+        {new Array(2).fill('').map(()=>{
+          return(
+        <Box  w={'48%'}  bg="primary.box"  my={'3'}  rounded={'md'}  borderBottomColor={'primary.border'} flexDirection={'column'} pt={'20px'} pb={'14px'} px={'16px'}> 
+          <Text position={'absolute'} textTransform={'uppercase'} zIndex={'9999'} top={'8'} left={'5'}  fontSize="xs" fontWeight={'semibold'} bg={'black'} borderRadius={'13px'} px={'10px'} py={'4px'}>popular</Text>
+
+        <Image 
+        position={'relative'}
+        w={'289px'}
+        borderRadius={'10px'}
+        height={'140px'}
+        source={{
+      uri: "https://wallpaperaccess.com/full/317501.jpg"
+    }} alt="Alternate Text"  />
+    <View flexDirection={'row'} alignItems={'center'} justifyContent={'space-between'} pt={'18px'} pb={'5px'}>
+      <Text  fontSize="sm" fontWeight={'medium'}>By : Doug Rose</Text>
+    <Text opacity={'1'} fontSize="xs" fontWeight={'medium'} color={'primary.text'} my={'4px'} bg="primary.box" w={'55px'} borderRadius={'13px'} textAlign={'center'} p={'1'}>42 min</Text>
+    </View>
+      <Text  noOfLines={2} fontSize="lg" fontWeight={'semibold'} textBreakStrategy='highQuality'>How finding and sharing information
+      {"\n"}
+       online has evolved…</Text>
+
+        </Box>
+          )
+        })}
+        </HStack>}
       </Container>
 
     </>
