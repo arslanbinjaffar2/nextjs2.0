c932478826e38643616bcd261b651c1c75977e86
Arslan jaffar <aja@eventbuizz.com>
2024-04-04 14:14:44 +0500

    WA20-1201

--- application/utils/DynamicIcon.tsx
+++ application/utils/DynamicIcon.tsx
@@ -47,6 +47,9 @@ import checkin_agendas from 'application/assets/icons/checkin_agendas';
 import business from 'application/assets/icons/IcoNetworkInterest';
 import documents from 'application/assets/icons/document'
 import download from 'application/assets/icons/download'
+import checkcircle from 'application/assets/icons/checkcircle'
+import cancelcircle from 'application/assets/icons/cancelcircle'
+import close from 'application/assets/icons/close'
 type IconProps = {
     name?: string;
     color?: string;
@@ -56,7 +59,9 @@ type IconProps = {
 };
 
 type IconType = 'IcoMyEvents' |'download' | 'logout' | 'myevents' |'documents' | 'myquestions' | 'editprofile'| 'emailmynotes'| 'mykeywords' | 'my_reservations'|'hdquestions' | 'myexhibitors' |'subregistration'|
-'mysponsers'| 'attendees' | 'chat' | 'checkIn' | 'ddirectory' | 'exhibitors' | 'plans' | 'help_desk' | 'homeMyevents' | 'gallery' | 'practical_info' | 'general_info' | 'additional_info' | 'information_pages' | 'maps' | 'my_attendee_list' | 'mydocuments' | 'my_notes' | 'myprograms' | 'alerts' | 'agendas' | 'myturnlist' | 'social_media' | 'social_wall' | 'speakers' | 'sponsors' | 'upcomingEvents' | 'qa' | 'survey' | 'polls' | 'attendee_authority' | 'checkin_agendas' | 'my_registrations' | 'business' | any;
+'mysponsers'| 'attendees' | 'chat' | 'checkIn' | 'ddirectory' | 'exhibitors' | 'plans' | 'help_desk' | 'homeMyevents' | 'gallery' | 'practical_info' | 'general_info' | 'additional_info' | 'information_pages' | 'maps' | 'my_attendee_list' | 'mydocuments' | 'my_notes' | 'myprograms' | 'alerts' | 'agendas' | 'myturnlist' | 'social_media' | 'social_wall' | 'speakers' | 'sponsors' | 'upcomingEvents' | 'qa' | 'survey' | 'polls' | 'attendee_authority' | 'checkin_agendas' | 'my_registrations' | 'business'
+|'checkcircle'|'cancelcircle' | 'close'
+;
 
 type Props = {
     iconType: IconType;
@@ -66,6 +71,9 @@ type Props = {
 const DynamicIcon: React.FC<Props> = ({ iconType, iconProps }) => {
 
     const iconMap: Record<IconType, ComponentType<IconProps>> = {
+        close,
+        checkcircle,
+        cancelcircle,
         IcoMyEvents,
         download,
         hdquestions,
