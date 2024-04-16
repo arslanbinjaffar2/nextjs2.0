b2842616447475aac56bef99c5ccab7d8ceab212
M Irfan <mir@eventbuizz.com>
2024-04-16 15:28:23 +0500

    WA20-664

--- application/components/atoms/polls/questions/web/DateTimeAnswer.tsx
+++ application/components/atoms/polls/questions/web/DateTimeAnswer.tsx
@@ -5,7 +5,7 @@ import { Question, FormData } from 'application/models/poll/Detail';
 import DateTimePicker from 'application/components/atoms/DateTimePicker';
 import moment from 'moment';
 import { Platform } from 'react-native';
-import {GENERAL_DATE_FORMAT, GENERAL_DATETIME_FORMAT} from 'application/utils/Globals'
+import {GENERAL_DATE_FORMAT, GENERAL_DATETIME_FORMAT, GENERAL_TIME_FORMAT} from 'application/utils/Globals'
 
 type PropTypes = {
   question: Question,
@@ -23,7 +23,7 @@ const DateAnswer = ({ question, formData, updateFormData, labels, error }: PropT
       <Box zIndex={9999} position={'relative'} mb="3" py="3" px="4" w="100%">
         <Text fontWeight="600" mb="3" maxW="80%" fontSize="lg">{question?.info?.question} {question?.required_question == '1' && <Text display={Platform.OS === 'web' ? "inline" : 'flex'} color="red.500">*</Text>}</Text>
         <Divider mb="5" opacity={0.27} bg="primary.text" />
-        <DateTimePicker label={GENERAL_DATETIME_FORMAT} showtime={`HH:mm:ss`} showdate={GENERAL_DATE_FORMAT} value={formData[question.id]?.answer ?? ''} onChange={(currentDate:any)=>{updateFormData(question.id, question.question_type, currentDate._isAMomentObject !== undefined && currentDate._isAMomentObject === true ? moment(currentDate).format(GENERAL_DATETIME_FORMAT) : '')}} />
+        <DateTimePicker label={GENERAL_DATETIME_FORMAT} showtime={GENERAL_TIME_FORMAT} showdate={GENERAL_DATE_FORMAT} value={formData[question.id]?.answer ?? ''} onChange={(currentDate:any)=>{updateFormData(question.id, question.question_type, currentDate._isAMomentObject !== undefined && currentDate._isAMomentObject === true ? moment(currentDate).format(GENERAL_DATETIME_FORMAT) : '')}} />
       </Box>
       {error && <Box  mb="3" py="3" px="4" backgroundColor="red.100" w="100%">
               <Text color="red.900"> {error} </Text>
