8fd7f8e53a1b8f7a8adebc03e4be67071041fdde
M Irfan <mir@eventbuizz.com>
2024-04-19 11:24:20 +0500

    WA20-736

---
+++ application/screens/web/reservation/Index.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Master from 'application/screens/web/layouts/Master';
+import AttendeeBooking from 'application/components/templates/reservation/web/Booking';
+import { Text } from 'native-base';
+
+const Index = () => {
+
+  return (
+      <>
+      <Text fontSize="xs">Hello</Text>
+      
+      </>
+  );
+
+};
+
+export default Index;
