4bd88e5fd41ac1727004c835284c34c62fc2446d
hashmat_eventbuizz <hab@eventbuizz.com>
2024-04-22 19:11:25 +0500

    WA20-1493

--- application/components/templates/programs/web/Detail.tsx
+++ application/components/templates/programs/web/Detail.tsx
@@ -174,9 +174,9 @@ const Detail = () => {
                         </Text>
                     </DetailBlock>
                     <Container mb="3" maxW="100%" w="100%">
-                        <HStack mb="3" space={0} overflow={'hidden'} flexWrap={'wrap'} rounded={8} justifyContent="flex-start" w="100%">
+                        <HStack mb="3" space={1} overflow={'hidden'} flexWrap={'wrap'} rounded={8} justifyContent="flex-start" w="100%">
                             {tabs.map((mtab: any, key: number) => (
-                                <Button key={mtab[0]} flex={1} rounded={0} minW={'50%'} onPress={() => setTab(mtab[0])} borderWidth="1px" py={0} borderColor="primary.darkbox" h="42px" bg={tab === mtab[0] ? 'primary.darkbox' : 'primary.box'} _text={{ fontWeight: '600' }}>{mtab[1]}</Button>
+                                <Button key={mtab[0]} flex={1} rounded={0} minW={'calc(50% - 2px)'} onPress={() => setTab(mtab[0])} borderWidth="0" py={0} borderColor="primary.boxbutton" h="42px" bg={tab === mtab[0] ? 'primary.boxbutton' : 'primary.box'} _text={{ fontWeight: '600' }}>{mtab[1]}</Button>
                             ))}
                         </HStack>
                         {group_id > 0 && (
