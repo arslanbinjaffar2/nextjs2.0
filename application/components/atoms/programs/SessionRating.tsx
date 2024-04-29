4c203a7c5c82adba021f6729794053c3c51f3b50
Hamza Latif <hla@eventbuizz.com>
2024-04-29 11:55:04 +0500

    WA20-1176

--- application/components/atoms/programs/SessionRating.tsx
+++ application/components/atoms/programs/SessionRating.tsx
@@ -5,6 +5,7 @@ import UseProgramService from 'application/store/services/UseProgramService';
 import DynamicIcon from 'application/utils/DynamicIcon';
 import UseLoadingService from 'application/store/services/UseLoadingService';
 import in_array from "in_array";
+import UseEventService from '../../../store/services/UseEventService'
 
 type AppProps = {
   program_id: any,
@@ -14,7 +15,7 @@ const SessionRating = ({program_id}:AppProps) => {
   const { detail,rating,SaveRating,FetchRating } = UseProgramService();
   const { processing } = UseLoadingService();
   const [rate, setRate] = React.useState(0);
-
+  const { event  } = UseEventService();
   useEffect(()=>{
     if(detail.program !== undefined){
       FetchRating({program_id:program_id ?? 0});
@@ -58,7 +59,7 @@ const SessionRating = ({program_id}:AppProps) => {
           <Box p="0" w="100%" bg={'primary.box'} mb={5} rounded={8}>
               <HStack px="3" py="1" bg="primary.darkbox" w="100%" space="3" alignItems="center" roundedTop={8}>
                   <DynamicIcon iconType={'my_notes'} iconProps={{ width: 15, height: 18 }} />
-                  <Text fontSize="lg">Rating</Text>
+                  <Text fontSize="lg">{event?.labels?.PROGRAM_RATING}</Text>
               </HStack>
               <Box py="3" px="4" w="100%">
              <Text> rating: {rate}</Text>
