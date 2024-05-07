50e06f24ead343df002227932a904088173b1af7
M Irfan <mir@eventbuizz.com>
2024-05-07 17:42:14 +0500

    WA20-1513

--- application/store/sagas/Banner.Saga.ts
+++ application/store/sagas/Banner.Saga.ts
@@ -6,7 +6,7 @@ import { getBannerApi } from 'application/store/api/Banner.Api'
 
 import { BannerActions } from 'application/store/slices/Banner.Slice'
 
-import { LoadingActions } from 'application/store/slices/Loading.Slice'
+import { LoadingActions, processing } from 'application/store/slices/Loading.Slice'
 
 import { HttpResponse } from 'application/models/GeneralResponse'
 
@@ -18,7 +18,6 @@ function* OnFetchBanners({
     type: typeof BannerActions.FetchBanners
 }): SagaIterator {
     const state = yield select(state => state);
-    if(state.banners.fetch_count > 1) return;  
     yield put(LoadingActions.addProcess({process:'banner-listing'}))
     const response: HttpResponse = yield call(getBannerApi, {}, state)
     yield put(BannerActions.update({ banners: response.data.data.banners!, banner_setting:response.data.data.banner_setting! }))
