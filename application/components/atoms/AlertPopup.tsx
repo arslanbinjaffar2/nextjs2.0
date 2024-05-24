023c40c3a4adb6d95c174857a8894320136b87cd
hashmat_eventbuizz <hab@eventbuizz.com>
2024-05-24 15:13:24 +0500

    APP-712

--- application/components/atoms/AlertPopup.tsx
+++ application/components/atoms/AlertPopup.tsx
@@ -1,4 +1,4 @@
-import { AlertDialog, Button, Center, Container, HStack, Text, Spacer, Wrap, Box } from "native-base";
+import { AlertDialog, Button, Center, Container, HStack, Text, Spacer, Wrap, Box, View } from "native-base";
 import React from 'react';
 import IcoNewsUpdate from "application/assets/icons/IcoNewsUpdate";
 
@@ -6,32 +6,35 @@ const AlertPopup = ({ isOpen, onClose, btnLeftFunc, btnRightFunc, cancelRef, tit
 
     return <AlertDialog  size={'lg'} leastDestructiveRef={cancelRef} isOpen={isOpen} onClose={onClose}>
           <AlertDialog.Content bg={'primary.boxsolid'}>
-            <AlertDialog.Header borderColor={'primary.bordercolor'} bg={'transparent'} fontWeight={600}>
+            <AlertDialog.Header borderColor={'transparent'} bg={'transparent'} fontWeight={600}>
               <HStack  space="0" alignItems="center">
                <Center w="40px">
                 <IcoNewsUpdate width={25} height={22} /> 
                </Center>
                
-               <Text pl={3} w={'calc(100% - 80px)'} fontSize="lg" fontWeight={600}>{title}</Text>
+               <Text pl={3} w={'calc(100% - 80px)'} fontSize="24px" fontWeight={600}>{title}</Text>
                <Spacer />
               <AlertDialog.CloseButton right={0} top={'-5px'} />
               </HStack>
             </AlertDialog.Header>
-            {text && <AlertDialog.Body bg={'transparent'}>
-              <Text  fontSize="md">{text}</Text>
+            {text && <AlertDialog.Body pt={0} bg={'transparent'}>
+              <View minHeight={90}>
+                <Text  fontSize="md">{text}</Text>
+              </View>
+              
               
             </AlertDialog.Body>}
-            <AlertDialog.Footer borderColor={'primary.bordercolor'} flexDirection={'column'} display={'flex'}  justifyContent={'flex-start'} p={0} bg={'transparent'}>
+            <AlertDialog.Footer borderColor={'primary.popupbordercolor'} flexDirection={'column'} display={'flex'}  justifyContent={'flex-start'} p={0} bg={'transparent'}>
               <Button.Group flexWrap="wrap" variant={'unstyled'} space={0}>
                 {btnRightText && <Box flex={1}>
                   <Button  _text={{fontSize: 'xl',textTransform: 'uppercase'}} fontWeight={500} bg={'none'} maxWidth={'256px'} w="100%" rounded={0} variant="unstyled"  onPress={btnRightFunc}>
-                  <Text isTruncated fontSize='xl'textTransform= 'uppercase' w={'225px'}>
+                  <Text textAlign={'center'} isTruncated fontSize='xl'textTransform= 'uppercase' w={'225px'}>
                     {btnRightText}
                     </Text>
                   </Button>
                 </Box>
                 }
-              {btnLeftText  && <Box borderLeftWidth={btnRightText ? '1':'0'} borderLeftColor={'primary.bordercolor'} flex={1}>
+              {btnLeftText  && <Box borderLeftWidth={btnRightText ? '1':'0'} borderLeftColor={'primary.popupbordercolor'} flex={1}>
                 <Button _text={{fontSize: 'xl',textTransform: 'uppercase'}}  bg={'none'}  w="100%" rounded={0} variant="unstyled"  fontWeight={500} onPress={btnLeftFunc} ref={cancelRef}>
                 <Text isTruncated fontSize='xl'textTransform= 'uppercase'  w={'225px'} textAlign={'center'} overflow={'hidden'}>
                   {btnLeftText}
