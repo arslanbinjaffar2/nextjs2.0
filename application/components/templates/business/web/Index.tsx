896c6bae47868c0e9d2ade7ec6d410c0da0843f7
Arslan jaffar <aja@eventbuizz.com>
2024-05-29 13:49:17 +0500

    feature/EB-21422

--- application/components/templates/business/web/Index.tsx
+++ application/components/templates/business/web/Index.tsx
@@ -116,7 +116,7 @@ const MatchedAttendeeList = ({ keywords, searchMatchAttendees, FetchSearchMatchA
       <HStack display={["block", "flex"]} mb="3" pt="2" w="100%" alignItems="center" justifyContent={'space-between'}>
         <Text fontSize="2xl">{modules?.find((attendees) => (attendees.alias == 'attendees'))?.name ?? ""}</Text>
         <View flexDirection={'row'} alignItems={'center'} w={['100%', '60%']} justifyContent={'space-between'}>
-          <Input rounded="10" w={['100%', '85%']} bg="primary.box" borderWidth={0}
+        <Input rounded="10" w={['88%', '90%']} bg="primary.box" borderWidth={0}
             borderColor={'transparent'}
             value={searchTerm} placeholder={event.labels?.GENERAL_SEARCH} onChangeText={(text: string) => {
               setSearchTerm(text);
