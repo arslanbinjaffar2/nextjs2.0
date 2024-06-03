57bb5c040465a6a7d96196bb04b08abea417981d
M Irfan <mir@eventbuizz.com>
2024-06-03 16:16:00 +0500

    APP-764

--- application/models/Event.ts
+++ application/models/Event.ts
@@ -372,6 +372,7 @@ interface SpeakerSettings {
   birth_date: number;
   spoken_languages: number;
   employment_date: number;
+  attendee_can_book_meeting_with_speaker: number;
 }
 
 export interface SponsorSettings {
