4341b15c022863085658cfd9fc7498efae39ad9b
M Irfan <mir@eventbuizz.com>
2024-06-04 17:43:21 +0500

    Fixed book meeting buttonn showing for speaker in mbile view

--- application/components/atoms/attendees/RectangleView.tsx
+++ application/components/atoms/attendees/RectangleView.tsx
@@ -127,6 +127,7 @@ const RectangleView = ({ border, attendee, speaker, disableMarkFavroute }: boxIt
                   {getPrivateFields(attendee)}
                 </Text>
               }
+              {isReservationModuleOn && isAppointmentTabEnabled && attendeeCanBookMeetingWithSpeaker && response?.data?.user?.id !== attendee?.id && (
                <Button
                   display={['','none']}
                     mt={2}
@@ -137,6 +138,7 @@ const RectangleView = ({ border, attendee, speaker, disableMarkFavroute }: boxIt
                     <Text textAlign="center" isTruncated width="95px">{event?.labels?.RESERVATION_BOOK_MEETING_LABEL}</Text>
                     
                   </Button>
+              )}
             </VStack>
             <Spacer />
             <HStack space="4" alignItems="center">
