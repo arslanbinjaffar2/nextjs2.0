c350f7e9c4b6f8f470e08b00486b76cf7ffe7c16
Arslan jaffar <aja@eventbuizz.com>
2024-06-04 17:48:31 +0500

    bugfix/APP-776

--- application/components/templates/business/web/Index.tsx
+++ application/components/templates/business/web/Index.tsx
@@ -16,6 +16,7 @@ import DynamicIcon from 'application/utils/DynamicIcon';
 import BannerAds from 'application/components/atoms/banners/BannerAds'
 import NextBreadcrumbs from 'application/components/atoms/NextBreadcrumbs';
 import in_array from "in_array";
+import { colors } from 'application/styles';
 const Index = () => {
   const { processing, loading } = UseLoadingService();
 
@@ -124,7 +125,7 @@ const MatchedAttendeeList = ({ keywords, searchMatchAttendees, FetchSearchMatchA
               setSearchTerm(text);
             }} leftElement={<Icon ml="2" color="primary.text" size="lg" as={AntDesign} name="search1" />} />
           <Pressable rounded="10" bg="primary.box" p={'8px'} onPress={() => setEnableFilter(true)}>
-            <DynamicIcon iconType={'attendee_Match'} iconProps={{ width: 20, height: 22, color: "primary.text" }} />
+            <DynamicIcon iconType={'attendee_Match'} iconProps={{ width: 20, height: 22, color:colors.text }} />
           </Pressable>
         </View>
       </HStack>
@@ -254,7 +255,7 @@ const ManageKeywords = ({ keywords, searchMatchAttendees, searchingAttendees, Fe
         <HStack mb="3" pt="2" w="100%" space="3" alignItems="center" justifyContent={'space-between'}>
           <Text fontSize="2xl">{modules?.find((network) => (network.alias == 'business'))?.name ?? ""}</Text>
           <Pressable rounded="10" bg="primary.500" p={'8px'} onPress={() => setEnableFilter(false)}>
-            <DynamicIcon iconType={'attendee_Match'} iconProps={{ width: 20, height: 22}} />
+            <DynamicIcon iconType={'attendee_Match'} iconProps={{ width: 20, height: 22,color:colors.text}} />
           </Pressable>
         </HStack>
         <HStack mx="-2" space="0" alignItems="center" flexWrap="wrap">
