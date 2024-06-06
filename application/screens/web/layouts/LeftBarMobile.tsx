dac401600dac41523e205e669c212bad3683887d
M Irfan <mir@eventbuizz.com>
2024-06-06 11:45:49 +0500

    APP-789

--- application/screens/web/layouts/LeftBarMobile.tsx
+++ application/screens/web/layouts/LeftBarMobile.tsx
@@ -135,7 +135,7 @@ const LeftBarMobile = ({setOpenMenu}:{setOpenMenu:any}) => {
               </HStack>
             </Pressable>
           )}
-          <Pressable
+          {/* <Pressable
             w="100%"
             px="4"
             py="2"
@@ -155,7 +155,7 @@ const LeftBarMobile = ({setOpenMenu}:{setOpenMenu:any}) => {
               </Center>
               <Text fontSize={'lg'} color="primary.text">Logout</Text>
             </HStack>
-          </Pressable>
+          </Pressable> */}
         </VStack>
     </ScrollView>
     </Center>
