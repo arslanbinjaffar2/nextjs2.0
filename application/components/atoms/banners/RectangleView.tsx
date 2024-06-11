89855a994e34b41b7a9dbdf1eb5965d8611cb123
hashmat_eventbuizz <hab@eventbuizz.com>
2024-06-11 17:47:51 +0500

    master

--- application/components/atoms/banners/RectangleView.tsx
+++ application/components/atoms/banners/RectangleView.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { Image } from 'native-base';
+import { useWindowDimensions } from 'react-native';
 
 const RectangleView = ({url}:{url:string}) => {
+    const { width } = useWindowDimensions();
     return (
         <Image
             source={{
@@ -10,7 +12,7 @@ const RectangleView = ({url}:{url:string}) => {
             alt=""
             resizeMode="cover"
             w="100%"
-            h={["82px","144px"]}
+            h={[(width - 30) * 0.24, 600 * 0.24]}
             rounded="10"
         />
     )
