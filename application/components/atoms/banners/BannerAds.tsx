f3f867937f411891314f71f86e011c47c5caf0b3
Arslan jaffar <aja@eventbuizz.com>
2024-06-14 13:13:25 +0500

    hotfix/EB-21515

--- application/components/atoms/banners/BannerAds.tsx
+++ application/components/atoms/banners/BannerAds.tsx
@@ -29,6 +29,7 @@ const BannerAds = ({
     const filtered = banners.filter((banner: Banner) => {
       if (module_name === 'dashboard') {
         var moduleType = banner.module_type.split(',');
+        console.log(moduleType,"moduleType")
         return (
           banner.module_name === module_name &&
           moduleType.includes(module_type ?? '')
@@ -137,8 +138,10 @@ const BannerAds = ({
     };
   }, [filteredBanners]);
   const currentBannerImage = filteredBanners[currentBanner]?.image;
+  console.log(filteredBanners,"filteredBanners")
   return (
-    <Box my="4" h={[(width - 30) * 0.24, 600 * 0.24]} position={'relative'} w={'100%'}  >
+    <>
+ {filteredBanners.length > 0 && <Box my="4" h={[(width - 30) * 0.24, 600 * 0.24]} position={'relative'} w={'100%'} >
       {filteredBanners.map((banner:Banner,index)=>
         <Container position={'absolute'} left={'0'} top={'0'} h={'100%'}  w="100%" maxW="100%">
           <Box width={"100%"}>
@@ -154,7 +157,8 @@ const BannerAds = ({
           </Box>
         </Container>
       )}
-    </Box>
+    </Box>}
+      </>
   );
 };
 
