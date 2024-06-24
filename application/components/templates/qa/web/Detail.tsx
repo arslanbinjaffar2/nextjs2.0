44c1fe3b646171b3a9d1efd6c253e1bce58d19e5
khawarr <khu@eventbuizz.com>
2024-06-24 18:12:33 +0500

    APP-892

--- application/components/templates/qa/web/Detail.tsx
+++ application/components/templates/qa/web/Detail.tsx
@@ -221,7 +221,7 @@ const Detail = () => {
         SubmitQa(postData);
 
         setAnonymously(false);
-        setParagraph(null);
+        setParagraph('');
         setLineNumber('');
         setQuestion('');
         setSpeaker(null);
