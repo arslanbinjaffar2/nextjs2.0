46ef7037798e7c040ee5aa5d5b98df4a336e5930
khawarr <khu@eventbuizz.com>
2024-06-26 11:38:57 +0500

    APP-905

--- application/screens/web/layouts/AuthLayout.tsx
+++ application/screens/web/layouts/AuthLayout.tsx
@@ -13,7 +13,7 @@ type Props = {
 
 const AuthLayout = ({ children }: Props) => {
 
-    const { event,loadSettingsModules } = UseEventService();
+    const { event,event_url,loadSettingsModules } = UseEventService();
 
     const { loadToken, isLoggedIn, getUser, response } = UseAuthService();
 
@@ -23,29 +23,14 @@ const AuthLayout = ({ children }: Props) => {
         getUser();
     }, [])
 
-    const checkUserGDPR = () => {
-        let requiredGDPR = event?.gdpr_settings?.enable_gdpr === 1 ? true : false;
-          if(requiredGDPR){
-              let userGDPRLogged = response?.data?.user?.gdpr_log;
-              if(!userGDPRLogged){
-                return false;
-              }
-          }
-          return true;
-    }
-
     React.useEffect(() => {
         if (isLoggedIn) {
-            if(checkUserGDPR() === false){
-                push(`/${event.url}/auth/gdpr`)
-            }else{
-                push(`/${event.url}/subRegistration`)
-            }
+            push(`/${event.url}/subRegistration`)
         }
     }, [isLoggedIn])
 
     React.useEffect(() => {
-        loadToken(Boolean(localStorage.getItem('access_token')));
+        loadToken(Boolean(localStorage.getItem(`access_token_${event_url}`)));
     }, [])
 
     return (
