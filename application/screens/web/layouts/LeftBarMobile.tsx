f3ef3fe21c0c68d0585c9f03ac4fbbd7a2ea9cf7
Eventbuizz <info@eventbuizz.com>
2024-06-26 04:25:42 -0600

    no message

--- application/screens/web/layouts/LeftBarMobile.tsx
+++ application/screens/web/layouts/LeftBarMobile.tsx
@@ -118,13 +118,13 @@ const LeftBarMobile = ({setOpenMenu}:{setOpenMenu:any}) => {
                   }
                 } else if (in_array(row?.alias, ['information_pages'])) {
                   // setLoading(true);
-                  
                   if(row?.section_type === 'link') {
                     router.push(`${row?.url}`)
+                  } else if(row?.section_type === 'page') {
+                    router.push(`/${event.url}/information-pages/event-info-detail/${row?.id}`)
                   } else {
-                    router.push(`/${event.url}/information-pages${row?.section_type === 'child_section' ? '/sub' : ''}/${row?.id}`)
+                    router.push(`/${event.url}/information-pages/${row?.id}`)
                   }
-                  
                 } else if (row?.alias === 'my-registrations') {
                   router.push(`/${event.url}/attendees/detail/${response?.data?.user?.id}`)
                 } else {
