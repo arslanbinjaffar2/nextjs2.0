6b3838b308671a45e861737be8a6c1efadbcdbe0
hashmat_eventbuizz <hab@eventbuizz.com>
2024-07-11 17:24:19 +0500

    APP-326

--- application/components/templates/myTurnList/web/Show.tsx
+++ application/components/templates/myTurnList/web/Show.tsx
@@ -121,7 +121,7 @@ const ShowTurnList = () => {
                                     {event?.labels?.GENERAL_GDPR_ACCEPT_TEXT}
                                 </Text>
                                 <Button onPress={() => { push(`/${event.url}/settings/editprofile`) }} width={'20%'} bg={'primary.100'} rounded={'5px'} p={'2'}>
-                                    <Text fontWeight={'semibold'} fontSize={'md'} isTruncated textAlign={'center'}>{event?.labels?.GENERAL_EDIT_PROFILE}</Text>
+                                    <Text color={'primary.hovercolor'} fontWeight={'semibold'} fontSize={'md'} isTruncated textAlign={'center'}>{event?.labels?.GENERAL_EDIT_PROFILE}</Text>
                                 </Button>
                             </Box>
 
