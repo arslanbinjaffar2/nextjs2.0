6adfc8a1d05a8b40c21699e154263e3d745439d3
Arslan jaffar <aja@eventbuizz.com>
2024-08-23 17:53:55 +0500

    feature/APP-1151

--- application/utils/DynamicIcon.tsx
+++ application/utils/DynamicIcon.tsx
@@ -91,6 +91,9 @@ import hand from 'application/assets/icons/hand'
 import calender_icon from 'application/assets/icons/calender_icon'
 import clock_icon from 'application/assets/icons/clock_icon'
 import forbidden_icon from 'application/assets/icons/forbidden_icon'
+import facebook from 'application/assets/icons/facebook'
+import linkedin from 'application/assets/icons/linkedin'
+import xtwiiter from 'application/assets/icons/xtwiiter'
 type IconProps = {
     name?: string;
     color?: string;
@@ -99,7 +102,7 @@ type IconProps = {
     height?: number;
 };
 
-type IconType ='forbidden_icon'|'qrcode'| 'IcoMyEvents'|'infobooth' |'download' | 'logout' | 'myevents' |'document' | 'myquestions' | 'edit_account'| 'email_icon'| 'match' | 'my_reservations'|'hdquestions' | 'myexhibitors' |'subregistration'|
+type IconType ='xtwitter'| 'linkedin' |'facebook'|'forbidden_icon'|'qrcode'| 'IcoMyEvents'|'infobooth' |'download' | 'logout' | 'myevents' |'document' | 'myquestions' | 'edit_account'| 'email_icon'| 'match' | 'my_reservations'|'hdquestions' | 'myexhibitors' |'subregistration'|
 'fav_sponsors'| 'attendees' | 'myattendees' | 'message' | 'chat' | 'checkIn' | 'ddirectory' | 'exhibitors' | 'plans' | 'help_desk' | 'homeMyevents' | 'gallery' | 'practical_info' | 'general_info' | 'additional_info' | 'information_pages' | 'maps' | 'my_attendee_list' | 'mydocuments' | 'notes' | 'myagendas' | 'myprograms' | 'alerts' | 'agendas' | 'myturnlist' | 'social' | 'social_wall' | 'speakers' | 'sponsors' | 'upcomingEvents' | 'qa' | 'survey' | 'polls' | 'attendee_authority' | 'checkin_agendas' | 'checkin' | 'my_registrations' | 'business' | 'request_to_speak' | 'editprofile' | 'mykeywords' | 'myreservation' | 'my_notes' | 'upcoming_events' | 'reservation' | 'certificate' | 'editnotes' | 'mybookings' 
 |'checkcircle'|'cancelcircle' | 'close' | 'edit_profile' | 'myPollResults' | 'mySurveyResults' | 'my_sub_registrations' | 'network_interest' | 'livesurveys'| 'attendee_Match' | 'star'| 'staro'| 'save' | 'edit_order' | 'Notattending'|'register' | 'delete_icon'| 'share_account'|'download_2'| 'email' |'share'| 'dropdown' | 'upload' | 'calender_icon'| 'clock_icon' | 'hand' | any;
 
@@ -201,7 +204,10 @@ const DynamicIcon: React.FC<Props> = ({ iconType, iconProps }) => {
         hand,
         calender_icon,
         clock_icon,
-        forbidden_icon
+        forbidden_icon,
+        facebook,
+        linkedin,
+        xtwiiter
     };
 
     const IconComponent = iconMap[iconType];
